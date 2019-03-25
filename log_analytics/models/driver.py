54dfec93c7e609e905e58a6c3014e4afe54010a8
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-24 23:18:17 -0300

    add a property for return the race time

--- log_analytics/models/driver.py
+++ log_analytics/models/driver.py
@@ -2,6 +2,16 @@ class Driver:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.laps = []
+
+    @property
+    def race_time(self):
+        time = 0
+        for lap in self.laps:
+            _minutes, _seconds = lap.lap_time.split(':')
+            time += int(_minutes)*60 + float(_seconds)
+        minutes, seconds = divmod(time, 60)
+        return "%02d:%f" % (minutes, seconds)
 
     def __repr__(self):
         return self.name
--- tests/log_analytics/models/test_driver.py
+++ tests/log_analytics/models/test_driver.py
@@ -1,4 +1,5 @@
 from log_analytics.models.driver import Driver
+from log_analytics.models.lap import Lap
 
 
 def test_driver_object():
@@ -6,3 +7,12 @@ def test_driver_object():
     assert driver.name == 'R.BARRICHELLO'
     assert driver.id == '033'
     assert driver.__repr__() == 'R.BARRICHELLO'
+
+
+def test_driver_race_time():
+    driver = Driver('033', 'R.BARRICHELLO')
+    driver.laps = [
+        Lap('1', '1:03.716', '43,675'),
+        Lap('2', '1:03.716', '43,675')
+    ]
+    assert driver.race_time == '02:7.432000'
