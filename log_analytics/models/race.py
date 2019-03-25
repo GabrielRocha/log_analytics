af61bc149f80afd3fba682445c455b8227458b83
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-24 23:49:56 -0300

    refactor the build_drivers and add result for list the drivers position on race

--- log_analytics/models/race.py
+++ log_analytics/models/race.py
@@ -12,15 +12,27 @@ class Race:
     def _build_drivers(self):
         self.drivers = {}
         for lap in self.laps:
-            driver = Driver(*lap[0].split(' – '))
+            driver_id, driver_name = lap[0].split(' – ')
             lap = Lap(*lap[1:])
-            if driver.id not in self.drivers:
+            if driver_id not in self.drivers:
+                driver = Driver(driver_id, driver_name)
                 driver.laps.append(lap)
-                self.drivers[driver.id] = driver
+                self.drivers[driver_id] = driver
             else:
-                self.drivers[driver.id].laps.append(lap)
+                self.drivers[driver_id].laps.append(lap)
             if lap.number == '4':
                 break
+
+    def result(self):
+        final_positions = []
+        drivers = list(self.drivers.values())
+        lap = self.FINAL_LAP
+        while lap > 0 and drivers:
+            positions = [driver for driver in drivers if len(driver.laps) == lap]
+            final_positions += sorted(positions, key=lambda x: x.race_time)
+            [drivers.remove(driver) for driver in positions]  # Remove the driver that finished the race
+            lap -= 1
+        return enumerate(final_positions, start=1)
 
     def __repr__(self):
         return f'Race - {id(self)}'
