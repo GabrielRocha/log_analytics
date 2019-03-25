9b97214c6c45eadb7fdbb970c8fa0d3414940f9c
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-25 20:32:37 -0300

    add more records on the race.log example for improving logs

--- tests/log_analytics/test_analysis.py
+++ tests/log_analytics/test_analysis.py
@@ -10,10 +10,10 @@ def test_read_log(dir_file):
 def test_final_result(dir_file):
     file = f'{dir_file}/resource/race.log'
     race = analysis.build_race(file)
-    assert len(race.laps) == 2
+    assert len(race.laps) == 8
     assert race.drivers['033'].name == 'R.BARRICHELLO'
     assert race.drivers['033'].id == '033'
-    assert race.drivers['033'].laps[0].number == '3'
+    assert race.drivers['033'].laps[0].number == '1'
     assert race.drivers['033'].laps[0].lap_time == '1:03.716'
     assert race.drivers['033'].laps[0].average_speed == '43,675'
-    assert race.drivers['033'].race_time == '02:7.432000'
+    assert race.drivers['033'].race_time == '04:12.864000'
