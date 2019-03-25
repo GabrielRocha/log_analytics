32c09b267ef73da9c07dee434a6c8face909aa4f
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-24 23:46:06 -0300

    generate the drivers and add his laps on the laps attribute until the first driver complete the 4 laps

--- log_analytics/models/race.py
+++ log_analytics/models/race.py
@@ -1,10 +1,26 @@
+from log_analytics.models.driver import Driver
+from log_analytics.models.lap import Lap
+
+
 class Race:
+    FINAL_LAP = 4
+
     def __init__(self, laps):
         self.laps = laps
+        self._build_drivers()
 
-    @property
-    def max_laps(self):
-        return max([lap.number for lap in self.laps])
+    def _build_drivers(self):
+        self.drivers = {}
+        for lap in self.laps:
+            driver = Driver(*lap[0].split(' – '))
+            lap = Lap(*lap[1:])
+            if driver.id not in self.drivers:
+                driver.laps.append(lap)
+                self.drivers[driver.id] = driver
+            else:
+                self.drivers[driver.id].laps.append(lap)
+            if lap.number == '4':
+                break
 
     def __repr__(self):
-        return f'Race - {self.max_laps} Laps'
+        return f'Race - {id(self)}'
--- tests/log_analytics/models/test_race.py
+++ tests/log_analytics/models/test_race.py
@@ -1,16 +1,31 @@
-from log_analytics.models.lap import Lap
 from log_analytics.models.race import Race
 
 
 def test_race_object():
-    lap = Lap('23:51:18.576', '033 – R.BARRICHELLO', '1', '1:03.716', '43,675')
-    race = Race([lap])
+    lap = [['033 – R.BARRICHELLO', '1', '1:03.716', '43,675']]
+    race = Race(lap)
     assert len(race.laps) == 1
-    assert race.laps[0].date_time == '23:51:18.576'
-    assert race.laps[0].driver.name == 'R.BARRICHELLO'
-    assert race.laps[0].driver.id == '033'
-    assert race.laps[0].number == '1'
-    assert race.laps[0].lap_time == '1:03.716'
-    assert race.laps[0].average_speed == '43,675'
-    assert race.max_laps == '1'
-    assert race.__repr__() == 'Race - 1 Laps'
+    assert race.drivers['033'].name == 'R.BARRICHELLO'
+    assert race.drivers['033'].id == '033'
+    assert race.drivers['033'].laps[0].number == '1'
+    assert race.drivers['033'].laps[0].lap_time == '1:03.716'
+    assert race.drivers['033'].laps[0].average_speed == '43,675'
+    assert race.__repr__() == f'Race - {id(race)}'
+
+
+def test_race_object_with_laps_less_than_4():
+    lap = [
+        ['033 – R.BARRICHELLO', '1', '1:03.716', '43,675'],
+        ['038 – F.MASSA', '1', '2:03.716', '43,675'],
+        ['033 – R.BARRICHELLO', '4', '1:03.716', '43,675'],
+        ['038 – F.MASSA', '2', '1:03.716', '43,675'],
+    ]
+    race = Race(lap)
+    assert len(race.laps) == 4
+    assert race.drivers['033'].name == 'R.BARRICHELLO'
+    assert race.drivers['033'].id == '033'
+    assert len(race.drivers['033'].laps) == 2
+    assert race.drivers['038'].name == 'F.MASSA'
+    assert race.drivers['038'].id == '038'
+    assert len(race.drivers['038'].laps) == 1
+    assert race.drivers['038'].race_time == '02:3.716000'
