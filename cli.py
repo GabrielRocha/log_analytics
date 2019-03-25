ddcaa9eef5cbc5bba94faddd06f803d06714a545
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-25 00:22:57 -0300

    list the race result by bash command

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+from tabulate import tabulate
 
 from log_analytics import analysis
 
@@ -12,6 +13,26 @@ def main():
 @click.option('--file', help='File name with path')
 def show_log(file):
     click.echo(analysis.read_log(file))
+
+
+@main.command(help='The final result of race')
+@click.option('--file', help='File name with path')
+def race_result(file):
+    result = [
+        (position, driver.id, driver.name, len(driver.laps), driver.race_time)
+        for position, driver in analysis.build_race(file).result()
+    ]
+    click.echo(tabulate(
+        result,
+        headers=[
+            '**Posição Chegada**',
+            '**Código Piloto**',
+            '**Nome Piloto**',
+            '**Qtde Voltas Completadas**',
+            '**Tempo Total de Prova**'
+        ],
+        numalign='center'
+    ))
 
 
 if __name__ == '__main__':
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,18 @@
 import cli
 
 
-def test_show_log_content(runner, dir_file):
+def test_show_log_content_command(runner, dir_file):
     result = runner.invoke(cli.show_log, ['--file', f'{dir_file}/resource/simple_log'])
     assert result.exit_code == 0
     assert result.output == 'Here\n'
+
+
+def test_race_result_command(runner, dir_file):
+    result = runner.invoke(cli.race_result, ['--file', f'{dir_file}/resource/race.log'])
+    assert result.exit_code == 0
+    assert result.output == ' **Posição Chegada**    **Código Piloto**   **Nome Piloto**   ' \
+                            '  **Qtde Voltas Completadas**   **Tempo Total de Prova**\n' \
+                            '---------------------  -------------------  ----------------- ' \
+                            ' -----------------------------  --------------------------\n' \
+                            '          1                    033          R.BARRICHELLO          ' \
+                            '          2                02:7.432000\n'
