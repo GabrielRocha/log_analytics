195c7116f497dd466b3ed0e3938750ec4180528f
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-25 00:13:23 -0300

    move build_race to analysis

--- log_analytics/helper.py
+++ log_analytics/helper.py
@@ -1,14 +1,6 @@
 import re
 
-from log_analytics.models.race import Race
-
 COLUMNS_REGEX = re.compile(r'  +|\t+ +|\t+')
-
-
-def build_race(content):
-    rows = content.split('\n')
-    laps = [split_log_to_columns(row)[1:] for row in rows[1:] if row]
-    return Race(laps)
 
 
 def split_log_to_columns(item):
--- tests/log_analytics/test_helper.py
+++ tests/log_analytics/test_helper.py
@@ -1,6 +1,6 @@
 import pytest
 
-from log_analytics.helper import build_race, split_log_to_columns
+from log_analytics.helper import split_log_to_columns
 
 
 @pytest.mark.parametrize('line', [
@@ -12,14 +12,3 @@ from log_analytics.helper import build_race, split_log_to_columns
 def test_split_log_to_columns(line):
     new_list = split_log_to_columns(line)
     assert new_list == ['23:51:18.576', '033 – R.BARRICHELLO', '3', '1:03.716', '43,675']
-
-
-def test_build_race():
-    content = '\n23:51:18.576      033 – R.BARRICHELLO\t\t          3\t\t1:03.716                        43,675\n'
-    race = build_race(content)
-    assert len(race.laps) == 1
-    assert race.drivers['033'].name == 'R.BARRICHELLO'
-    assert race.drivers['033'].id == '033'
-    assert race.drivers['033'].laps[0].number == '3'
-    assert race.drivers['033'].laps[0].lap_time == '1:03.716'
-    assert race.drivers['033'].laps[0].average_speed == '43,675'
