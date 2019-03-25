26665b572c1b1c66c08aabfa37f6b022476c0ccb
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-25 00:29:30 -0300

    add a property for return the lap time in seconds

--- log_analytics/models/lap.py
+++ log_analytics/models/lap.py
@@ -4,5 +4,10 @@ class Lap:
         self.lap_time = lap_time
         self.average_speed = average_speed
 
+    @property
+    def lap_time_seconds(self):
+        _minutes, _seconds = self.lap_time.split(':')
+        return int(_minutes) * 60 + float(_seconds)
+
     def __repr__(self):
         return f'{self.number} - {self.lap_time}'
--- tests/log_analytics/models/test_lap.py
+++ tests/log_analytics/models/test_lap.py
@@ -7,3 +7,8 @@ def test_lap_object():
     assert lap.lap_time == '1:03.716'
     assert lap.average_speed == '43,675'
     assert lap.__repr__() == '3 - 1:03.716'
+
+
+def test_lap_time_seconds():
+    lap = Lap('3', '1:03.716', '43,675')
+    assert lap.lap_time_seconds == 63.716
