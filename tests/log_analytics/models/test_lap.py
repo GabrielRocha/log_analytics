723b2399c711a784feefd6bfb061c1e428c9a4b7
Gabriel Rocha <gabriel.rocha@catho.com>
2019-03-24 23:17:05 -0300

    remove the driver and datetime value because now the lap belong to driver

--- tests/log_analytics/models/test_lap.py
+++ tests/log_analytics/models/test_lap.py
@@ -2,11 +2,8 @@ from log_analytics.models.lap import Lap
 
 
 def test_lap_object():
-    lap = Lap('23:51:18.576', '033 – R.BARRICHELLO', '3', '1:03.716', '43,675')
-    assert lap.date_time == '23:51:18.576'
-    assert lap.driver.name == 'R.BARRICHELLO'
-    assert lap.driver.id == '033'
+    lap = Lap('3', '1:03.716', '43,675')
     assert lap.number == '3'
     assert lap.lap_time == '1:03.716'
     assert lap.average_speed == '43,675'
-    assert lap.__repr__() == '3 - R.BARRICHELLO - 1:03.716'
+    assert lap.__repr__() == '3 - 1:03.716'
